9da9893b26bf4aac109b47df5d0ccef1ac8e4da4
brynnpeirce <103545682+brynnpeirce@users.noreply.github.com>
2022-08-15 14:32:49 -0700

    Guessing game console app updated
    added while loop

---
+++ Practice/Practice/Practice/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool isGuessed = false;
+
+            do
+            {
+                Console.WriteLine("Guess what letter my name starts with!");
+                string letter = Console.ReadLine();
+
+                switch (letter)
+                {
+                    case "X":
+                        Console.WriteLine("you Guess X. Try again");
+                        Console.WriteLine("Guess again!");
+                        Console.ReadLine();
+                        break;
+                    case "L":
+                        Console.WriteLine("You guessed L. Try again");
+                        Console.WriteLine("Guess again!");
+                        Console.ReadLine();
+                        break;
+                    case "M":
+                        Console.WriteLine("You guessed M. Try again");
+                        Console.WriteLine("Guess again!");
+                        Console.ReadLine();
+                        break;
+                    case "B":
+                        Console.WriteLine("you guessed B. Correct!");
+                        isGuessed = true;
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong");
+                        Console.WriteLine("Guess again!");
+                        Console.ReadLine();
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+
+           
+
+            
+            bool colorguess = false;
+
+            while (!colorguess)
+            {
+                Console.WriteLine("Guess my favorite color!");
+                string color = Console.ReadLine();
+                switch (color)
+                {
+                    case "Red":
+                        Console.WriteLine("Nope! Red is not my favorite color. Try again!");
+                        Console.ReadLine();
+                        break;
+                    case "Orange":
+                        Console.WriteLine("Nope! Orange is not my favorite color. Try again!");
+                        Console.ReadLine();
+                        break;
+                    case "Yellow":
+                        Console.WriteLine("Nope! Yellow is not my favorite color. Try again!");
+                        Console.ReadLine();
+                        break;
+                    case "Green":
+                        Console.WriteLine("Nope! Green is not my favorite color. Try again!");
+                        Console.ReadLine();
+                        break;
+                    case "Blue":
+                        Console.WriteLine("Nope! Blue is not my favorite color. Try again!");
+                        Console.ReadLine();
+                        break;
+                    case "Purple":
+                        colorguess = true;
+                        Console.WriteLine("Yes! I love Purple");
+                        Console.ReadLine();
+                        break;
+            
+
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
