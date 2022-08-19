8d87daec16158ed2d186fd3953eb1ffd1539ef8b
brynnpeirce <brynn98607@gmail.com>
2022-08-19 16:45:58 -0700

    strings and integers assignment
    exception handeling
    try/catch

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> Numbers = new List<int>() { 22, 44, 76, 98, 100 };
+                Console.WriteLine("Pick a number: ");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in Numbers)
+                {
+                    Console.WriteLine(number / userNumber);
+                }
+                Console.ReadLine();
+            }
+
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
