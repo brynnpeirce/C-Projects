8dedb4d0c0ad33629153cb443f80689e1f5590f1
brynnpeirce <brynn98607@gmail.com>
2022-08-24 12:32:46 -0700

    MainMethod assignment
    instantiating classes and calling methods

---
+++ MainMethodApp/MainMethodApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            theMath math = new theMath(); //instantiate class
+
+            
+            Console.WriteLine(math.MathOperation(5)); //calling method and passing in an integer
+            Console.WriteLine(math.MathOperation(2.2m)); //calling method and passing in a decimal
+            Console.WriteLine(math.MathOperation("6"));// calling method and passing in a string
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ MainMethodApp/MainMethodApp/theMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    public class theMath
+    {
+        public int MathOperation(int Num)
+        {
+            int addMath = Num + 2; //create a math operation w/ integer and return as int
+            return addMath;
+        }
+
+        public int MathOperation(decimal Num)
+        {
+            decimal Math = Num - 2;//takes a decimal, creates a different math operation, return as int
+            int subtractMath = Convert.ToInt32(Math);
+            return subtractMath;
+        }
+
+        public int MathOperation(string Num)
+        {
+            int Math = Convert.ToInt32(Num);
+            int multiplyMath = Math * 2;  //Convert string to integer, create different math operation, return as int
+            return multiplyMath;
+        }
+    }
+}
