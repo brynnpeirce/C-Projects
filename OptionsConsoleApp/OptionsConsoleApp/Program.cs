90da2e392833b0b2cf0c535e3535546df91966ab
brynnpeirce <brynn98607@gmail.com>
2022-08-24 22:27:03 -0700

    Options ConsoleApp
    Optional parameters

---
+++ OptionsConsoleApp/OptionsConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myMath math = new myMath(); //instantiate class
+
+            Console.WriteLine("Would you like to enter 2 numbers or 1 number?");
+            string userAnswer = Console.ReadLine();
+
+            if (userAnswer == "1")
+            {
+                Console.WriteLine("Enter a number: "); //Asking user for input
+                string userInput = Console.ReadLine();//saving input as a string
+                int Num = Convert.ToInt32(userInput);//converting input to an int
+                Console.WriteLine(math.MathOp(Num));
+            }
+            if (userAnswer == "2")
+            {
+                Console.WriteLine("Enter 1st number: "); //Asking user for input
+                string userInput = Console.ReadLine();//saving input as a string
+                int Num = Convert.ToInt32(userInput);//converting input to an int
+                Console.WriteLine("Enter a 2nd number: ");
+                string userInput2 = Console.ReadLine();
+                int Num2 = Convert.ToInt32(userInput2);
+                Console.WriteLine(math.MathOp(Num, Num2));
+            }
+            if (userAnswer != "1" && userAnswer != "2")
+            {
+                Console.WriteLine("Please enter 1 or 2");
+            }
+
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
---
+++ OptionsConsoleApp/OptionsConsoleApp/myMath.cs
@@ -0,0 +1,11 @@
+﻿namespace OptionsConsoleApp
+{
+    public class myMath
+    {
+        public int MathOp( int num1, int num2 = 1) //two parameters, num2 is optional, default num2 is 1
+        {
+               int addMath = num1 + num2;
+                return addMath;
+        }
+    }
+}
