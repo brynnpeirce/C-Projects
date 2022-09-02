f779ad419dc6be70b4778e83e8215373eeba3e48
brynnpeirce <brynn98607@gmail.com>
2022-09-02 13:34:01 -0700

    Assignment overloading operators
    overloading operators

---
+++ Job/Job/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            if (emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Job/Job/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Id = 1;
+            emp1.FirstName = "Brynn";
+            emp1.LastName = "Jackson";
+
+            Employee emp2 = new Employee();
+            emp2.Id = 2;
+            emp2.FirstName = "Macgregor";
+            emp2.LastName = "Peirce";
+
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("Employee 1 is the same as Employee 2");
+            }
+            else
+            {
+                Console.WriteLine("Employee 1 is not the same as Employee 2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
