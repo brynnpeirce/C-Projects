6436073deab9206d2e22d3ebdc113bfac237fd3c
brynnpeirce <brynn98607@gmail.com>
2022-09-08 00:29:34 -0700

    Lots of updates
    TwentyOne
    TwentyOne2
    Constructors

---
+++ Constructors/Constructors/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Job
+    {
+        public string JobTitle;
+        public int Salary;
+
+
+        // Chain constructors
+        public Job(string JobTitle) : this(JobTitle, 60000)
+        {
+
+        }
+
+        public Job(String JobTitle, int Salary)
+        {
+
+        }
+        
+
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Brynn Pierce"; //using a const variable
+            Console.WriteLine("My name is {0}.", myName);
+
+            var Age = 23; //using var variable
+            Console.WriteLine("I am {0} years old.", Age);
+
+        }
+
+    }
+}
